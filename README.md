70610cad91b191ef60354e05abb12259d8819eb4
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2024-07-29 19:05:37 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Desafio-Java-DIO-Banco-Digital
+ Desafio do projeto 03 do bootCamp Java da Dio
---
+++ banco-digital-2024/src/main/java/com/pedroMonteiro/Endereco.java
@@ -0,0 +1,14 @@
+package com.pedroMonteiro;
+
+import lombok.AllArgsConstructor;
+
+
+
+
+@AllArgsConstructor
+public class Endereco {
+    private String endereco;
+    private String bairro;
+    private String cidade;
+    private String uf;
+}
